7a039fa993769522f6860edbd115f63ea4804ed3
DioxusGrow <dioxusgrow@proton.me>
2024-12-23 09:17:06 +0300

    first commit

---
+++ realtor_files/59d77d2728668835758b45c9.js
@@ -0,0 +1,11 @@
+// @copyright MegaGroup © 2012 - 2018, megagroup.ru
+// 2023-06-02 11:51:40
+// D: false 7 7
+(function (o,n,c) {
+  'use strict';
+  o[c] = o[c] || function() {(o[c].x=o[c].x||[]).push(arguments)};
+  var s = n.createElement('script'); s.type = 'text/javascript'; s.charset="utf-8";
+  s.async = true; s.src = "https://client.onicon.ru/visitor.js?v=4.4.92";
+  o[c]('init', {ver:'4.4.92',lang:'ru',staticClient: 'https://client.onicon.ru',siteId:'59d77d2728668835758b45c9',server:'wss://n3.onicon.ru',initTime:new Date()});
+  n.getElementsByTagName('head')[0].appendChild(s);
+})(window, document, 'Onicon');
